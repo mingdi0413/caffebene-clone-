f98a95ba25a176aa70ccc6fdfd52c1cffdf7de39
mingdi0413 <117414860+mingdi0413@users.noreply.github.com>
2022-12-23 12:44:05 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,51 +1,54 @@
-// $(document).ready(function (){
-//     var current = 0; 
-//     var setIntervalId;
-// $('.main_btn > li').eq(0).find('a').addClass('on');
-//     var idx = 0;
-//     console.log(idx);
-// $('.main_btn > li').click(function (e) {
-//     e.preventDefault();
-//     var i = $(this).index();
-//     console.log(i);
-//     $('.main_btn > li').find('a').removeClass('on');
-//     $(this).find('a').addClass('on');
-//     move(i); 
-//     });
+$(document).ready(function(){
+    var current = 0; 
+    var setIntervalId;
+    $('.m_btn li').eq(0).addClass('on');
 
-//     $('.list li').on({
-//         mouseover: function () {
-//             clearInterval(setIntervalId);
-//         },
-//         mouseout: function () {
-//             timer();
-//         }
-//     })
-// })
-// timer();
-// function timer() {
-// current = 0;
-// setIntervalId = setInterval(function () {
-//     var n = current + 1; 
-//     if (n == 3) {
-//         n = 0;
-//     }
+    $(".m_btn li").click(function (e){
+        e.preventDefault();
+       
+        var i = $(this).index();
+        move(i);
 
-//     move(n);
-// }, 5000)
+      
 
-// }
-// function move(i) {
-// if (i == current) return;
+    })
 
-// var currentEl = $('.list li').eq(current);
+    $('#main_img').on({
+        mouseover: function (){
+            clearInterval(setIntervalId);
+        },
+        mouseout: function () {
+            timer ()
+        }
+    })
 
-// var nextEl = $('.list li').eq(i);
+    timer();
+    function timer(){
+       
+       setIntervalId = setInterval(function (){
+            var n = current + 1; 
+            if (n == 3) {
+            n = 0;
+        }
+        move(n); 
+    
+        },3000);
+    }
+    function move(i) {
+     
+        if(i == current) return;
 
-// currentEl.css({ top: 0 }).animate({ top: "720px" });
-// nextEl.css({ top: "720px" }).animate({ top: "0" });
-// current = i;
+         
+        var currentEl =  $('.list ul li').eq(current);
 
-// $('.main_btn li').removeClass('on');
-// $('.main_btn li').eq(i).addClass('on');
-// }
+       
+        var nextEl = $('.list ul li').eq(i);
+
+        currentEl.css({left : '0%'}).animate({left : '-100%'})
+        nextEl.css({left : '100%'}).animate({left : '0%'})
+        current = i;
+      
+        $('.m_btn li a').removeClass('on')
+        $('.m_btn li a').eq(i).addClass('on')
+    }
+});
